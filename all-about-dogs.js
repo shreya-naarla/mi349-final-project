fdca5031d9addb863e18dcd203998f57482d613d
shreya-naarla <naarlash@msu.edu>
2024-12-08 22:09:42 +0000

    commit

--- all-about-dogs.js
+++ all-about-dogs.js
@@ -24,20 +24,20 @@ function description6() {
   document.getElementById('german').remove();
 }
 
-//food-chart.html
+
 //index.html
 function readMore() {
-  var dots = document.getElementById("dots");
+  var halfway = document.getElementById("halfway");
   var moreText = document.getElementById("more");
   var btnText = document.getElementById("myBtn");
   
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
+  if (halfway.style.display === "none") {
+    halfway.style.display = "inline";
     btnText.innerHTML = "Read more"; 
     moreText.style.display = "none";
   } 
   else {
-    dots.style.display = "none";
+    halfway.style.display = "none";
     btnText.innerHTML = "Read less"; 
     moreText.style.display = "inline";
   }
@@ -45,8 +45,6 @@ function readMore() {
 
 
 //age-calc.html
-    
-  
 function CalculateAge(){
   
   var userDateinput = document.getElementById("dob").value;  
